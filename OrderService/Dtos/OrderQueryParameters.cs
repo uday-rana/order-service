69472c97d2585bde4748b9ce86dd8d0113e8865e
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-06-12 19:56:16 -0400

    Add range validation to page and page size

--- OrderService/Dtos/OrderQueryParameters.cs
+++ OrderService/Dtos/OrderQueryParameters.cs
@@ -1,17 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 using OrderService.Models;
 
 namespace OrderService.Dtos;
 
 public record OrderQueryParameters(
+    [Range(1, int.MaxValue)]
     int Page = 1,
+
+    [Range(1, 100)]
     int PageSize = 10,
+
     int? CustomerId = null,
+
     OrderStatus? Status = null,
+
     DateTime? CreatedFrom = null,
+
     DateTime? CreatedTo = null,
+
     DateTime? FulfilledFrom = null,
+
     DateTime? FulfilledTo = null,
+
     int? ProductId = null,
+
     string? SortBy = null,
+
     bool SortDescending = false
 );
